6eceb69c29b223c69d95530652ccdc478edb05c7
Kyoko Kobe <49667068+kobekyoko@users.noreply.github.com>
2019-05-10 15:47:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,4 @@ setTimeout(() =>{
 	console.log(a+b);
 }, 3000) //return the answer of a + b after 3 seconds
 
-console.log(__dirname); //tells you directry
+console.log(__dirname); //tells your directry
